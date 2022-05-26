cb8bfc5b8469fb49d0975b87532a0454805cacd0
Kevin Smith <102421938+ksmith94@users.noreply.github.com>
2022-05-26 11:36:38 -0400

    Numbers

--- playground/types.js
+++ playground/types.js
@@ -1,26 +1,44 @@
-const name = 'kevin';
-const middle = "donald";
-const last = `smith`;
+// debugger;
+// const name = 'kevin';
+// const middle = "donald";
+// const last = `smith`;
 
-const sentence = "she's so cool";
-const sentence2 = `she's so "cool"`;
+// const sentence = "she's so cool";
+// const sentence2 = `she's so "cool"`;
 
-const song = `Ohhh
+// const song = `Ohhh
 
-ya 
+// ya 
 
-I like 
-pizza`;
+// I like 
+// pizza`;
 
 // const hello = "Hello, my name is " + name + ". Nice to meet you.";
 // let hello2 = 'Hello, my name is ';
 // hello2 = hello2 + name;
 // hello2 = hello2 + '. Nice to meet you.';
-const hello = `hello, my name is ${name}. Nice to meet you. I am ${25 + 2} years old`;
-const html = `
-    <div>
-        <h2>${name}</h2>
-        <p>${hello}</p>
-    </div>
-    `;
-document.body.innerHTML = html;
+
+// const hello = `hello, my name is ${name}. Nice to meet you. I am ${25 + 2} years old`;
+
+// const html = `
+//     <div>
+//         <h2>${name}</h2>
+//         <p>${hello}</p>
+//     </div>
+//     `;
+// document.body.innerHTML = html;
+
+const age = 100;
+const name = 'wes';
+
+// const a = 10;
+// const b = 20;
+
+const smarties = 20;
+const kids = 3;
+const eachKidGets = Math.floor(smarties / kids);
+const dadGets = smarties % kids;
+console.log(`Each kid gets ${eachKidGets}`);
+
+
+const price = 1034;
