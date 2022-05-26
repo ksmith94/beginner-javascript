c4de8f066f970350ba5aa8a6c56834e151f75103
Kevin Smith <102421938+ksmith94@users.noreply.github.com>
2022-05-26 14:14:58 -0400

    Parameters and Arguments

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,12 +1,31 @@
 //function definition
-function calculateBill() {
+function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) {
     //this is the function body
     console.log('Running Calculate Bill!');
-    const total = 100 * 1.13;
+    const total = billAmount + billAmount * taxRate + billAmount * tipRate;
     return total;
 }
 
 //function call or **Run**
-const myTotal = calculateBill();
-//console.log(`Your total is $${myTotal}`);
-console.log(`Your total is $${calculateBill()}`);
+const wesTotal = 500;
+const wesTaxRate = 0.3;
+//const myTotal = calculateBill(wesTotal, wesTaxRate);
+
+//function definition
+function sayHiTo(firstName) {
+    return `Hello ${firstName}`;
+}
+
+//const greeting = sayHiTo('wes');
+// console.log(greeting);  
+
+function doctorize(name) {
+    return `Dr. ${name}`;
+}
+
+function yell(name = '') {
+    return `HEY ${name.toUpperCase()}`;
+}
+
+const myBill4 = calculateBill(100, undefined, 0.2);
+console.log(myBill4);
