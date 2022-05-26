8b534cf448de3b6a020e1cfbf04973ba0c6b3a0f
Kevin Smith <102421938+ksmith94@users.noreply.github.com>
2022-05-26 12:02:42 -0400

    null and undefined

--- playground/types.js
+++ playground/types.js
@@ -28,17 +28,41 @@
 //     `;
 // document.body.innerHTML = html;
 
-const age = 100;
-const name = 'wes';
+// const age = 100;
+// const name = 'wes';
 
 // const a = 10;
 // const b = 20;
 
-const smarties = 20;
-const kids = 3;
-const eachKidGets = Math.floor(smarties / kids);
-const dadGets = smarties % kids;
-console.log(`Each kid gets ${eachKidGets}`);
+// const smarties = 20;
+// const kids = 3;
+// const eachKidGets = Math.floor(smarties / kids);
+// const dadGets = smarties % kids;
+// console.log(`Each kid gets ${eachKidGets}`);
 
 
-const price = 1034;
+// const price = 1034;
+
+const person = {
+    first: 'wes',
+    last: 'bos',
+    age: 100
+};
+
+let dog;
+console.log(dog);
+dog = 'snickers';
+
+let somethingUndefined; 
+const somethingNull = null;
+
+const cher = {
+    first: 'cher',
+};
+
+const teller = {
+    first: 'Raymond',
+    last: 'Teller'
+}
+teller.first = 'Teller';
+teller.last = null;
