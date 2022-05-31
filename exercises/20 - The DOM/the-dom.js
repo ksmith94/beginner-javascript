838fcefde9c3d4e275c6e3ec20f971fbaa884dbe
Kevin Smith <102421938+ksmith94@users.noreply.github.com>
2022-05-31 13:32:03 -0400

    The DOM - Selecting Elements

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,6 @@
+const p = document.querySelector('p');
+const imgs = document.querySelectorAll('.item img');
+const item2 = document.querySelector('.item2');
+const item2Image = item2.querySelector('img');
+console.log(item2Image);
+
