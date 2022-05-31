e0457d159c87e7739d8da6725dd848b20655894e
Kevin Smith <102421938+ksmith94@users.noreply.github.com>
2022-05-31 13:54:00 -0400

    The DOM - Element Properties and Methods

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -2,5 +2,21 @@ const p = document.querySelector('p');
 const imgs = document.querySelectorAll('.item img');
 const item2 = document.querySelector('.item2');
 const item2Image = item2.querySelector('img');
-console.log(item2Image);
+const heading = document.querySelector('h2');
 
+console.log(heading.textContent);
+console.log(heading.innerText);
+//set the text content on that element
+// heading.textContent = 'Wes is cool';
+// console.log(heading.textContent);
+// console.log(heading.innerText);
+
+console.log(heading.innerHTML);
+console.log(heading.outerHTML);
+
+const pizzaList = document.querySelector('.pizza');
+console.log(pizzaList.textContent);
+
+// pizzaList.textContent = `${pizzaList.textContent} +pizza`;
+pizzaList.insertAdjacentText('afterbegin', `pizza`);
+pizzaList.insertAdjacentText('beforeend', `pizza`);
