9e5408afbbeacdb45540317c8fed67994b6d6ae9
Kevin Smith <102421938+ksmith94@users.noreply.github.com>
2022-05-31 14:12:03 -0400

    The DOM - Working with Classes

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,22 +1,35 @@
-const p = document.querySelector('p');
-const imgs = document.querySelectorAll('.item img');
-const item2 = document.querySelector('.item2');
-const item2Image = item2.querySelector('img');
-const heading = document.querySelector('h2');
+// const p = document.querySelector('p');
+// const imgs = document.querySelectorAll('.item img');
+// const item2 = document.querySelector('.item2');
+// const item2Image = item2.querySelector('img');
+// const heading = document.querySelector('h2');
 
-console.log(heading.textContent);
-console.log(heading.innerText);
-//set the text content on that element
-// heading.textContent = 'Wes is cool';
 // console.log(heading.textContent);
 // console.log(heading.innerText);
+// //set the text content on that element
+// // heading.textContent = 'Wes is cool';
+// // console.log(heading.textContent);
+// // console.log(heading.innerText);
 
-console.log(heading.innerHTML);
-console.log(heading.outerHTML);
+// console.log(heading.innerHTML);
+// console.log(heading.outerHTML);
 
-const pizzaList = document.querySelector('.pizza');
-console.log(pizzaList.textContent);
+// const pizzaList = document.querySelector('.pizza');
+// console.log(pizzaList.textContent);
 
-// pizzaList.textContent = `${pizzaList.textContent} +pizza`;
-pizzaList.insertAdjacentText('afterbegin', `pizza`);
-pizzaList.insertAdjacentText('beforeend', `pizza`);
+// // pizzaList.textContent = `${pizzaList.textContent} +pizza`;
+// pizzaList.insertAdjacentText('afterbegin', `pizza`);
+// pizzaList.insertAdjacentText('beforeend', `pizza`);
+
+
+//classes!
+const pic = document.querySelector('.nice');
+pic.classList.add('open');
+pic.classList.remove('cool');
+console.log(pic.classList);
+
+function toggleRound() {
+    pic.classList.toggle('round');
+}
+
+pic.addEventListener('click', toggleRound);
