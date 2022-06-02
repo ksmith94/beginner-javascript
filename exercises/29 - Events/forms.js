706b766688f08b788381f449471a1b0770a0ceea
Kevin Smith <102421938+ksmith94@users.noreply.github.com>
2022-06-02 08:26:08 -0400

    Events - Prevent Defaul and Form Events

---
+++ exercises/29 - Events/forms.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+const wes = document.querySelector('.wes');
+
+wes.addEventListener('click', (event) => {
+  const shouldChangePage = confirm(
+    'This website might be malicious. Do you wish to proceed?',
+  );
+  if (!shouldChangePage) {
+    event.preventDefault();
+  }
+});
+
+const signupForm = document.querySelector('[name="signup"]');
+
+signupForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const name = event.currentTarget.name.value;
+  if (name.includes('chad')) {
+    alert('Sorry Bro');
+    event.preventDefault();
+  }
+});
+
+function logEvent(event) {
+  console.log(event.type);
+  console.log(event.currentTarget.value);
+}
+signupForm.name.addEventListener('keyup', logEvent);
+signupForm.name.addEventListener('keydown', logEvent);
+signupForm.name.addEventListener('focus', logEvent);
+signupForm.name.addEventListener('blur', logEvent);
+// 'keyup'
+// 'keydown'
+// 'focus'
+// 'blur'
