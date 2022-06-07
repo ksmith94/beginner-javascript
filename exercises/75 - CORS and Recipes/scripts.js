3dc81a65528229b7018251eca6855343d6f7b933
Kevin Smith <102421938+ksmith94@users.noreply.github.com>
2022-06-07 10:46:02 -0400

    Recipe API - UI

--- exercises/75 - CORS and Recipes/scripts.js
+++ exercises/75 - CORS and Recipes/scripts.js
@@ -7,12 +7,44 @@ Please use this replacement API URL "https://recipes.beginnerjavascript.com/api"
 
 const baseEndpoint = 'https://recipes.beginnerjavascript.com/api';
 const proxy = 'https://cors-anywhere.herokuapp.com/';
+const $form = document.querySelector('form.search');
+const $recipesGrid = document.querySelector('.recipes');
 
 async function fetchRecipes(query) {
   // https://cors-anywhere.herokuapp.com/https://recipes.beginnerjavascript.com/api?q=pizza
   const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
   const data = await res.json();
-  console.log(data);
+  return data;
 }
 
-fetchRecipes('pizza');
+function displayRecipes(recipes) {
+  console.log('creating html', recipes);
+  const html = recipes.map(
+    (recipe) => `<div class="recipe">
+    <h2>${recipe.title}</h2>
+    <p>${recipe.ingredients}</p>
+    ${recipe.thumbnail
+    && `<img src="${recipe.thumbnail}" alt="${recipe.title}"/>`}
+    <a href="${recipe.href}" target="_blank">View Recipe</a>
+    </div>`
+  );
+  $recipesGrid.innerHTML = html.join('');
+}
+
+async function fetchAndDisplay(query) {
+  $form.submit.disabled = true;
+  const recipes = await fetchRecipes(query);
+  $form.submit.disabled = false;
+  displayRecipes(recipes.results);
+}
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const el = event.currentTarget;
+  console.log(event.currentTarget);
+  fetchAndDisplay(el.query.value);
+}
+
+$form.addEventListener('submit', handleSubmit);
+
+fetchAndDisplay('pizza');
