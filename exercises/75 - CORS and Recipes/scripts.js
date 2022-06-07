571db539d9e8b9a2ea37e8c7b4d047b9a0226b94
Kevin Smith <102421938+ksmith94@users.noreply.github.com>
2022-06-07 09:43:03 -0400

    What is CORS? Recipes API

--- exercises/75 - CORS and Recipes/scripts.js
+++ exercises/75 - CORS and Recipes/scripts.js
@@ -1,2 +1,18 @@
-// The Recipe Puppy API used in the course is broken
-// Please use this replacement API URL "https://recipes.beginnerjavascript.com/api"
+/* 1. The Recipe Puppy API used in the course is broken
+Please use this replacement API URL "https://recipes.beginnerjavascript.com/api"
+
+2. To use Cors anywhere, you now need to request temporary access.
+- You can do that here: https://cors-anywhere.herokuapp.com/corsdemo
+*/
+
+const baseEndpoint = 'https://recipes.beginnerjavascript.com/api';
+const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+async function fetchRecipes(query) {
+  // https://cors-anywhere.herokuapp.com/https://recipes.beginnerjavascript.com/api?q=pizza
+  const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
+  const data = await res.json();
+  console.log(data);
+}
+
+fetchRecipes('pizza');
